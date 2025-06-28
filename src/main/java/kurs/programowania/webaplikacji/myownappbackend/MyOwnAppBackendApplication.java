bdbf916944e617a1342e25d84a72e7e535695623
Piotr Góralski <piotrgoralski1997@gmail.com>
2025-06-28 14:02:31 +0200

    First commit with basic app

---
+++ src/main/java/kurs/programowania/webaplikacji/myownappbackend/MyOwnAppBackendApplication.java
@@ -0,0 +1,13 @@
+package kurs.programowania.webaplikacji.myownappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOwnAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kurs/programowania/webaplikacji/myownappbackend/MyOwnAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kurs.programowania.webaplikacji.myownappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
